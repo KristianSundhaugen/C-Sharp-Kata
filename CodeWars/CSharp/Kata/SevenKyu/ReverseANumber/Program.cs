ec6725a5f842a8e74a3f2c71e31e9c36464a975f
kristiansundhaugen95 <kristian.sundhaugen@schibsted.com>
2023-12-25 17:11:08 +0100

    Final solution for ReverseANumber

---
+++ CodeWars/CSharp/Kata/SevenKyu/ReverseANumber/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+    public static void Main()
+    {
+        var kata = new Kata();
+
+        // Test 1
+        int result1 = kata.ReverseNumber(123);
+        Console.WriteLine($"Test 1: {result1}");
+
+        // Test 2
+        int result2 = kata.ReverseNumber(-123);
+        Console.WriteLine($"Test 2: {result2}");
+
+        // Test 3
+        int result3 = kata.ReverseNumber(0);
+        Console.WriteLine($"Test 3: {result3}");
+    }
+
+    public int ReverseNumber(int n)
+    {
+        string reversedStringFinal;
+
+        if (n < 0)
+        {
+            n*=-1;
+            var reversedStringNumber = new string(n.ToString().Reverse().ToArray());
+            reversedStringFinal = "-" + reversedStringNumber;
+        }
+        else
+        {
+            reversedStringFinal = new string(n.ToString().Reverse().ToArray());
+        }
+
+        return int.Parse(reversedStringFinal);
+    }
+}

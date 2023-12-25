2d1c3385acb51b635f7016bd88ef42296b6d0d47
kristiansundhaugen95 <kristian.sundhaugen@schibsted.com>
2023-12-25 16:25:18 +0100

    First Kata Test Revers A Number

---
+++ CodeWars/C-Sharp/Kata/7kyu/Reverse-a-Number/KataTestf.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Linq;
+using NUnit.Framework;
+
+
+[TestFixture]
+public class KataTestf
+{
+    [Test]
+    public void _0_BasicTests()
+    {
+        var kata = new Kata();
+
+        Assert.AreEqual(321, kata.ReverseNumber(123));
+        Assert.AreEqual(-321, kata.ReverseNumber(-123));
+        Assert.AreEqual(0, kata.ReverseNumber(0));
+    }
+}

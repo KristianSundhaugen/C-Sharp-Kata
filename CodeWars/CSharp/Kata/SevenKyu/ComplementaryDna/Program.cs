75a88687e33350d2099f9287bf93d9ca5b084cf0
kristiansundhaugen95 <kristian.sundhaugen@schibsted.com>
2024-01-03 19:42:41 +0100

    Solved ComplementaryDna

--- CodeWars/CSharp/Kata/SevenKyu/ComplementaryDna/Program.cs
+++ CodeWars/CSharp/Kata/SevenKyu/ComplementaryDna/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+public class DnaStrand
+{
+    public static string MakeComplement(string dna)
+    {
+        List<char> newDnaList = new List<char>();
+
+        foreach (char item in dna)
+        {
+            switch (item)
+            {
+                case 'A':
+                    newDnaList.Add('T');
+                    break;
+                case 'T':
+                    newDnaList.Add('A');
+                    break;
+                case 'C':
+                    newDnaList.Add('G');
+                    break;
+                case 'G':
+                    newDnaList.Add('C');
+                    break;
+                default:
+                    // Handle other characters if needed
+                    break;
+            }
+        }
+
+        char[] newDnaArray = newDnaList.ToArray();
+        string complementaryDna = new string(newDnaArray);
+
+        return complementaryDna;
+    }
+}

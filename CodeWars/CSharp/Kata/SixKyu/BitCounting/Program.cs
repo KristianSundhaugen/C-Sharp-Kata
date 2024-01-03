96ac9e5d22e6d766e46325e60517a3b62adb952e
kristiansundhaugen95 <kristian.sundhaugen@schibsted.com>
2024-01-03 21:19:03 +0100

    Solved BitCounting

---
+++ CodeWars/CSharp/Kata/SixKyu/BitCounting/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Kata
+{
+    public static void Main()
+    {
+        Console.WriteLine(CountBits(1234));
+    }
+
+    public static int CountBits(int n)
+    {
+        if (n < 0)
+        {
+            return -1;
+        }
+
+        string binary = IntToBinary(n);
+
+        int amountOfBits = BinaryCounter(binary);
+
+        return amountOfBits;
+    }
+
+    private static string IntToBinary(int n)
+    {
+        return Convert.ToString(n, 2);
+    }
+
+    private static int BinaryCounter(string binaryString)
+    {
+        return binaryString.Count(c => c == '1');
+    }
+}

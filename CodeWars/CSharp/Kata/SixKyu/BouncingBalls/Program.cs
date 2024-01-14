4f1eb80329d14f5bb0fe13e551104b1e16c8741b
KristianSundhaugen <kristiansundhaugen@gmail.com>
2024-01-14 14:38:15 +0100

    Solved BouncingBall challange

---
+++ CodeWars/CSharp/Kata/SixKyu/BouncingBalls/Program.cs
@@ -0,0 +1,47 @@
+﻿public class Kata
+{
+    public static void Main()
+    {
+        Console.WriteLine(BouncingBall(3,0.66,1.5));
+        Console.WriteLine(BouncingBall(3,1,1.5));
+    }
+
+    public static int BouncingBall(double h, double bounce, double window)
+    {
+        if (h <= 0)
+        {
+            return -1;
+        }
+
+        if (bounce <= 0 || bounce >= 1)
+        {
+            return -1;
+        }
+
+        if (window >= h)
+        {
+            return -1;
+        }
+
+        double bounceHeight = h;
+        int seenBounces = 0;
+
+        do
+        {
+            bounceHeight *= bounce;
+
+            //Ball is falling
+            seenBounces++;
+
+            if (bounceHeight > window)
+            {
+                //Ball is passing window on the way up
+                seenBounces++;
+            }
+
+        } while (bounceHeight > window);
+
+
+        return seenBounces;
+    }
+}
